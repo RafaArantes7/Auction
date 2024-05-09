8827d049135396034d1baca11b9b2f474c8afe1d
RafaArantes7 <rafael.arantes7@hotmail.com>
2024-05-08 22:25:44 -0300

    zeppelin

---
+++ contracts/TokenZeppelin.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/AccessControl.sol";
+
+contract Token is ERC20, AccessControl {
+    
+    bytes32 public  constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    constructor(address minter) ERC20("Dradon ball Z", "DBZ") {
+        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Only minter");
+        _mint(to, amount);
+    }
+
+}
+
